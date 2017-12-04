4ce9b1f77d1047ae3665e91ef710c37e4d6d757b
andrejarboe <andrejarboe@gmail.com>
2017-12-03 21:33:06 -0500

    add space buttons to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,23 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+
+    /* Global varables 
+     **********************************/
+    var topics = ['space', 'stars'];
+
+    /* functions
+     *****************************/
+    function displayButtons() {
+        for(var i=0; i<topics.length; i++){
+            console.log(topics[i]);
+            var button = $('<button>');
+            button.text(topics[i]);
+            $('#space-buttons').append(button);
+        }
+    }
+    
+    /* Running code
+    ******************************/
+    displayButtons();
+
 });
