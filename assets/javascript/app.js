026473939cb52366bb92503cd747d26a729cc510
andrejarboe <andrejarboe@gmail.com>
2017-12-03 21:59:41 -0500

    add new button to screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,46 @@ $(document).ready(function () {
 
     /* Global varables 
      **********************************/
-    var topics = ['space', 'stars'];
+    var topics = ['space', 'stars', 'black hole'];
 
     /* functions
      *****************************/
     function displayButtons() {
+        //clear space buttons
+        $('#space-buttons').empty();
+
+        //make buttons
         for(var i=0; i<topics.length; i++){
             console.log(topics[i]);
             var button = $('<button>');
             button.text(topics[i]);
+            button.attr('data-name', topics[i]);
+            button.attr('class', 'button');
+
             $('#space-buttons').append(button);
         }
+    }
+
+    // make a new button
+    function newButton(){
+        $('#submit').on('click', function(){
+            topics.push($('#search').val()); 
+            displayButtons();           
+        });
+    }
+
+    //grab button data
+    function makeGifs(){
+
+    }
+
+    //start the site
+    function start(){
+        displayButtons();
+        newButton();
     }
     
     /* Running code
     ******************************/
-    displayButtons();
-
+    start();
 });
